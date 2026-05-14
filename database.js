840416606b92dc160333b901ea2323372808f2a3
Kaick <kaicknr@hotmail.com.br>
2026-05-14 09:33:06 -0300

    corrigindo database

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: "@db.jvgrkhkitozuuqkmzsfx.supabase.co:5432/postgres",
+ postgresql://postgres:Kaicknr$@db.jvgrkhkitozuuqkmzsfx.supabase.co:5432/postgres
   ssl: {
     rejectUnauthorized: false
   }
